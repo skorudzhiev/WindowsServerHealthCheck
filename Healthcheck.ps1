6c440611687a26ab422c1234099b9853f84224a7
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2018-12-14 21:59:04 +0200

    Add files via upload
    initial version working for a list of windows servers

---
+++ Healthcheck.ps1
@@ -0,0 +1,35 @@
+﻿### Healthcheck showing server Uptime, 3389 Port Status, TermService and ProfSvc status
+### To Run the script create file named "server-list.txt", 
+### containing the list of server names and place it in a directory of your choice. 
+### Change the directory path in the first and last line of the script.
+### Execute the script. When it is finished it will create results.txt file in the same directory
+
+$names = Get-Content "C:\Temp\sko\server-list.txt" 
+        @( 
+           foreach ($name in $names) 
+          { 
+            if ( Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue )  
+          { 
+            $wmi = gwmi Win32_OperatingSystem -computer $name 
+            $LBTime = $wmi.ConvertToDateTime($wmi.Lastbootuptime) 
+            [TimeSpan]$uptime = New-TimeSpan $LBTime $(get-date)
+
+            Write-output "$name" 
+            Write-output "Uptime is:  $($uptime.days) Days $($uptime.hours) Hours $($uptime.minutes) Minutes $($uptime.seconds) Seconds"
+            Write-output "`n"
+            $t = New-Object System.Net.Sockets.TcpClient "$name", 3389; 
+              if($t.Connected) 
+               {
+                "Port 3389 is Open and Connected"
+               }
+            $TermServiceStatus = (Get-Service -ComputerName "$name" -Name "TermService").Status
+            $UserProfileStatus = (Get-Service -ComputerName "$name" -Name "ProfSvc").Status
+            Write-Output "Remote Desktop Service is in Status: $TermServiceStatus" 
+            Write-Output "User Profile Service is in Status: $UserProfileStatus"
+            Write-Output "---------------------------------------------------------------------------------" 
+          } 
+             else { 
+                Write-output "$name is not pinging" 
+                  } 
+               } 
+         ) | Out-file -FilePath "C:\Temp\sko\results.txt"
